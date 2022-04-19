ddc0e5e55e450d87617f447123f4f8c0e5d200e7
davidcanas <dg0837.dc@gmail.com>
2022-04-19 20:18:59 +0100

    Ping command

--- src/commands/Info/avatar.ts
+++ src/commands/Info/avatar.ts
@@ -2,7 +2,7 @@ import Command from "../../structures/Command";
 import Client from "../../structures/Client";
 import CommandContext from "../../structures/CommandContext";
 
-export default class Help extends Command {
+export default class Avatar extends Command {
     constructor(client: Client) {
         super(client, {
             name: "avatar",
