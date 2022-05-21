ce277cb9c7fb60cf3c8f2560d365ea3280c6ff18
David <canas@danitto.live>
2022-05-21 17:22:07 +0100

    Optimize

--- src/commands/Util/render.ts
+++ src/commands/Util/render.ts
@@ -1,7 +1,8 @@
 import Command from "../../structures/Command";
 import Client from "../../structures/Client";
 import CommandContext from "../../structures/CommandContext";
-import { Type } from "../../structures/CommandContext"
+import { Type } from "../../structures/CommandContext";
+import { textChangeRangeIsUnchanged } from "typescript";
 export default class Render extends Command {
   constructor(client: Client) {
     super(client, {
@@ -16,60 +17,58 @@ export default class Render extends Command {
           description: "O link do website a renderizar",
           required: true,
         },
-       
       ],
     });
   }
 
   async execute(ctx: CommandContext): Promise<void> {
-    
     if (ctx.channel.type !== 0) return;
- 
-    if(ctx.channel.nsfw || this.client.allowedUsers.includes(ctx.author.id)) {
+
+    if (ctx.channel.nsfw || this.client.allowedUsers.includes(ctx.author.id)) {
       let msg;
-      let args = ctx.args[0]
-      if(!args) {
-        ctx.sendMessage("Precisas de explicitar o link do website a renderizar")
+      let args = ctx.args[0];
+      if (!args) {
+        ctx.sendMessage(
+          "Precisas de explicitar o link do website a renderizar"
+        );
         return;
-    }
-        if(!args.startsWith("http")) {
-          
-         args = "https://" + ctx.args[0]
-        }
+      }
+      if (!args.startsWith("http")) {
+        args = "https://" + ctx.args[0];
+      }
 
-        if(ctx.type !== Type.INTERACTION) {
-            
-            msg = await ctx.sendMessage("A renderizar...")
+      if (ctx.type !== Type.INTERACTION) {
+        msg = await ctx.sendMessage("A renderizar...");
+      } else {
+        ctx.defer();
+      }
+      const fetch = await this.client.fetch(
+        `https://image.thum.io/get/width/1200/crop/1600/${args}`
+      );
+
+      if (fetch.headers.get("content-type") !== "image/png") {
+        if (ctx.type !== Type.INTERACTION) {
+          msg.edit("Não consegui renderizar esse website");
         } else {
-         ctx.defer()
+          ctx.sendMessage("Não consegui renderizar esse website");
         }
-        const fetch = await this.client.fetch(`https://image.thum.io/get/maxAge/12/width/800/crop/1600/${ctx.args[0]}`)
-        if(fetch.text() === "Invalid url in thumbnail request.") {
-         if(ctx.type !== Type.INTERACTION) {
-           msg.edit("Não consegui renderizar esse website")
+        return;
+      }
+
+      setTimeout(async () => {
+        const embed = new this.client.embed()
+          .setTitle("Website Renderizado")
+          .setImage(`https://image.thum.io/get/width/1200/crop/1600/${args}`)
+          .setColor("RANDOM");
+
+        if (ctx.type !== Type.INTERACTION) {
+          msg.edit({ content: "", embed });
         } else {
-         ctx.sendMessage("Não consegui renderizar esse website")
+          ctx.sendMessage({ content: "", embed });
         }
-        return
-       }
-       var embed = new this.client.embed()
-       .setTitle("Website Renderizado")
-       .setImage(`https://image.thum.io/get/width/1200/crop/1600/${args}`)
-       .setColor("RANDOM")
-       
-       setTimeout(async () => {
-      
-       
-        if(ctx.type !== Type.INTERACTION) {
-          msg.edit({content: "", embed})
-        } else {
-          ctx.sendMessage({content: "", embed})
-        }
-    }, 5000);
+      }, 5000);
     } else {
-        ctx.sendMessage("Apenas podes usar este comando em um canal NSFW")
+      ctx.sendMessage("Apenas podes usar este comando em um canal NSFW");
     }
-    
-  
-
-  }}
+  }
+}
