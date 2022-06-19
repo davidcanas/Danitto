03e719bbd709ae77ad6738ad99fdd78e3220ec6b
David <canas@danitto.live>
2022-06-19 19:15:33 +0100

    Use normal eris version

--- src/commands/Owner/daniconfig.ts
+++ src/commands/Owner/daniconfig.ts
@@ -5,7 +5,7 @@ import {
   ActionRow,
   ActionRowComponents,
   ComponentInteraction,
-  InteractionComponentSelectMenuData,
+  ComponentInteractionSelectMenuData,
   Message,
 } from "eris";
 import { ComponentCollector } from "../../structures/Collector";
@@ -122,7 +122,7 @@ export default class DaniConfig extends Command {
       });
 
       collector.on("collect", async (i) => {
-        const data = i.data as InteractionComponentSelectMenuData;
+        const data = i.data as ComponentInteractionSelectMenuData;
         const value = data.values[0];
         msg.edit({
           content: `Desativei o commando ${value} com sucesso`,
@@ -176,7 +176,7 @@ export default class DaniConfig extends Command {
       });
 
       collector.on("collect", async (i) => {
-        const data = i.data as InteractionComponentSelectMenuData;
+        const data = i.data as ComponentInteractionSelectMenuData;
         const value = data.values[0];
         msg.edit({
           content: `Reativei o commando ${value} com sucesso`,
